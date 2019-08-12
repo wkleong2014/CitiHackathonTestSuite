90454250cc8d3d937117015b5b6f1f5cc87f7251
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2019-08-12 15:53:19 +0800

    Initial Commit

---
+++ src/main/java/citi/hackathon/CitiHackathonTestSuiteApplication.java
@@ -0,0 +1,13 @@
+package citi.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiHackathonTestSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiHackathonTestSuiteApplication.class, args);
+	}
+
+}
