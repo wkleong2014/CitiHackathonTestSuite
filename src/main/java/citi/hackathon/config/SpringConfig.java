dd88c894b2ddaf068ce5fcb6568526ff05aad0e3
Wei Kong <wkleong.2014@sis.smu.edu.sg>
2019-09-07 16:55:57 +0800

    Create authentication testsuite (#2)
    * Refactor and added various endpoints to test various modules, Added create admin account test
    
    * Added get test case for authentication
    
    * Refactoring codes
    
    * Simplify error message

--- src/main/java/citi/hackathon/config/SpringConfig.java
+++ src/main/java/citi/hackathon/config/SpringConfig.java
@@ -8,7 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class SpringConfig {
 	
-	private static String reportUrl = "http://localhost:8080";
+	private static String currentAuthenticationUrl;
+	private static String currentEventUrl;
+	private static String currentCommunicatorUrl;
+	private static String currentReportUrl;
 	
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -16,13 +19,36 @@ public class SpringConfig {
 	   return builder.build();
 	}
 
-	public static String getReportUrl() {
-		return reportUrl;
+	public static synchronized String getCurrentAuthenticationUrl() {
+		return currentAuthenticationUrl;
 	}
 
-	public static void setReportUrl(String reportUrl) {
-		SpringConfig.reportUrl = reportUrl;
+	public static synchronized void setCurrentAuthenticationUrl(String currentAuthenticationUrl) {
+		SpringConfig.currentAuthenticationUrl = currentAuthenticationUrl;
 	}
-	
+
+	public static synchronized String getCurrentEventUrl() {
+		return currentEventUrl;
+	}
+
+	public static synchronized void setCurrentEventUrl(String currentEventUrl) {
+		SpringConfig.currentEventUrl = currentEventUrl;
+	}
+
+	public static synchronized String getCurrentCommunicatorUrl() {
+		return currentCommunicatorUrl;
+	}
+
+	public static synchronized void setCurrentCommunicatorUrl(String currentCommunicatorUrl) {
+		SpringConfig.currentCommunicatorUrl = currentCommunicatorUrl;
+	}
+
+	public static synchronized String getCurrentReportUrl() {
+		return currentReportUrl;
+	}
+
+	public static synchronized void setCurrentReportUrl(String currentReportUrl) {
+		SpringConfig.currentReportUrl = currentReportUrl;
+	}
 	
 }
