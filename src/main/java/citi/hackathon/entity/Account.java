558ba2f4125fb02541acc05109210b30171a07ca
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2019-09-15 17:27:03 +0800

    Completed authentication test suite

--- src/main/java/citi/hackathon/entity/Account.java
+++ src/main/java/citi/hackathon/entity/Account.java
@@ -1,21 +1,23 @@
 package citi.hackathon.entity;
 
 public class Account {
-	
-	private int userId;
-	private String username;
-    private String password;
-    private String accountType;
-    private String emailAddress;
-    private String firstName;
-    private String lastName;
-    
-    // Default Empty Constructor
-    public Account() {}
 
-    // Parameterise Constructor
-	public Account(int userId, String username, String password, String accountType, String emailAddress,
-			String firstName, String lastName) {
+	private Integer userId;
+	private String username;
+	private String password;
+	private String accountType;
+	private String emailAddress;
+	private String firstName;
+	private String lastName;
+	private Boolean isAdmin;
+
+	// Default Empty Constructor
+	public Account() {
+	}
+
+	// Parameterise Constructor
+	public Account(Integer userId, String username, String password, String accountType, String emailAddress,
+			String firstName, String lastName, Boolean isAdmin) {
 		this.userId = userId;
 		this.username = username;
 		this.password = password;
@@ -23,13 +25,14 @@ public class Account {
 		this.emailAddress = emailAddress;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.isAdmin = isAdmin;
 	}
 
-	public int getUserId() {
+	public Integer getUserId() {
 		return userId;
 	}
 
-	public void setUserId(int userId) {
+	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
 
@@ -80,18 +83,20 @@ public class Account {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
 	@Override
 	public String toString() {
-		return "Account [userid=" + userId
-				+ ", username=" + username
-				+ ", password=" + password
-				+ ", accountType=" + accountType
-				+ ", emailAddress=" + emailAddress
-				+ ", firstName=" + firstName
-				+ ", lastName=" + lastName
-				+ "]";
+		return "Account [userId=" + userId + ", username=" + username + ", password=" + password + ", accountType="
+				+ accountType + ", emailAddress=" + emailAddress + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", isAdmin=" + isAdmin + "]";
 	}
-    
-    
+
 }
--- src/main/java/citi/hackathon/restcontroller/TestSuiteController.java
+++ src/main/java/citi/hackathon/restcontroller/TestSuiteController.java
@@ -5,7 +5,11 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,80 +22,120 @@ import citi.hackathon.CitiHackathonReportTestSuite;
 import citi.hackathon.config.SpringConfig;
 import citi.hackathon.entity.Account;
 import citi.hackathon.entity.JunitReport;
-import citi.hackathon.entity.Sample;
+import citi.hackathon.entity.ResultString;
+import citi.hackathon.entity.UpdateAccount;
 
 @RestController
 public class TestSuiteController {
 	private static final Logger LOG = LoggerFactory.getLogger(TestSuiteController.class);
-	
+
 	@RequestMapping("/test/authentication")
-	public JunitReport testAuthentication(@RequestParam(value="url") String url) {
+	public JunitReport testAuthentication(@RequestParam(value = "url") String url) {
 		JUnitCore junit = new JUnitCore();
 		SpringConfig.setCurrentAuthenticationUrl(url);
 		Result result = junit.run(ParallelComputer.methods(), CitiHackathonAuthenticationTestSuite.class);
-		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(), result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
+		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(),
+				result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
 		LOG.info(report.toString());
 		return report;
 	}
-	
+
 	@RequestMapping("/test/event")
-	public JunitReport testEvent(@RequestParam(value="url") String url) {
+	public JunitReport testEvent(@RequestParam(value = "url") String url) {
 		JUnitCore junit = new JUnitCore();
 		SpringConfig.setCurrentEventUrl(url);
 		Result result = junit.run(ParallelComputer.methods(), CitiHackathonEventTestSuite.class);
-		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(), result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
+		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(),
+				result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
 		LOG.info(report.toString());
 		return report;
 	}
-	
+
 	@RequestMapping("/test/report")
-	public JunitReport testReport(@RequestParam(value="url") String url) {
+	public JunitReport testReport(@RequestParam(value = "url") String url) {
 		JUnitCore junit = new JUnitCore();
 		SpringConfig.setCurrentReportUrl(url);
 		Result result = junit.run(ParallelComputer.methods(), CitiHackathonReportTestSuite.class);
-		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(), result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
+		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(),
+				result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
 		LOG.info(report.toString());
 		return report;
 	}
-	
+
 	@RequestMapping("/test/communicator")
-	public JunitReport testCommunicator(@RequestParam(value="url") String url) {
+	public JunitReport testCommunicator(@RequestParam(value = "url") String url) {
 		JUnitCore junit = new JUnitCore();
 		SpringConfig.setCurrentCommunicatorUrl(url);
 		Result result = junit.run(ParallelComputer.methods(), CitiHackathonCommunicatorTestSuite.class);
-		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(), result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
+		JunitReport report = new JunitReport(result.getRunCount(), result.getFailureCount(),
+				result.getRunCount() - result.getFailureCount() - result.getIgnoreCount(), result.getFailures());
 		LOG.info(report.toString());
 		return report;
 	}
-	
-	@RequestMapping("/sample")
-	public Sample sample() {
-		return new Sample("Sample Username", "Sample Password");
-	}
-	
-	@RequestMapping("/accounts")
-	public Account test_account(
-			@RequestParam("username") String username,
-			@RequestParam("password") String password,
-			@RequestParam("accountType") String accountType,
-			@RequestParam("emailAddress") String emailAddress,
-			@RequestParam("firstName") String firstName,
-			@RequestParam("lastName") String lastName
-			) {
-		Account account = new Account(1, username, password, accountType, emailAddress, firstName, lastName);
+
+	@GetMapping("/accounts")
+	public Account get_account(@RequestParam("userId") Integer userId) {
+		Account account = null;
+		if (userId == 1) {
+			account = new Account(1, "admin", "47b7bfb65fa83ac9a71dcb0f6296bb6e", "admin", "admin_nimda@email.com",
+					"Admin", "Nimda", true);
+		} else if (userId == 2) {
+			account = new Account(2, "peter", "47b7bfb65fa83ac9a71dcb0f6296bb6e", "user", "peter_johnson@email.com",
+					"Peter", "Johnson", false);
+		}
 		LOG.info("Returning Account: " + account.toString());
 		return account;
 	}
-	
-	@PostMapping("/sample_with_request_body")
-	public Sample sample_with_request_body(@RequestBody Sample sample) {
-		if (sample == null) {
-			return sample;
+
+	@PostMapping("/accounts")
+	public Account create_account(@RequestParam("username") String username, @RequestParam("password") String password,
+			@RequestParam("accountType") String accountType, @RequestParam("emailAddress") String emailAddress,
+			@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+		Account account = null;
+		if ("admin".equals(accountType)) {
+			account = new Account(1, username, password, accountType, emailAddress, firstName, lastName, true);
+		} else {
+			account = new Account(2, username, password, accountType, emailAddress, firstName, lastName, false);
 		}
-		sample.setUsername("updated_username");
-		sample.setPassword("updated_password");
-		return sample;
+		LOG.info("Create Account: " + account.toString());
+		return account;
+	}
+
+	@PutMapping("/accounts")
+	public Account update_admin_account(@RequestParam("userId") Integer userId,
+			@RequestBody UpdateAccount updateAccount) {
+		Account account = new Account(1, "admin", updateAccount.getPassword(), "admin", updateAccount.getEmailAddress(),
+				updateAccount.getFirstName(), updateAccount.getLastName(), true);
+		LOG.info("Update Account: " + account.toString());
+		return account;
+	}
+
+	@PatchMapping("/accounts")
+	public Account update_user_account(@RequestParam("userId") Integer userId,
+			@RequestBody UpdateAccount updateAccount) {
+		Account account = new Account(2, "peter", updateAccount.getPassword(), "user", updateAccount.getEmailAddress(),
+				updateAccount.getFirstName(), updateAccount.getLastName(), false);
+		LOG.info("Update Account: " + account.toString());
+		return account;
+	}
+
+	@DeleteMapping("/accounts")
+	public Account delete_account(@RequestParam("userId") Integer userId) {
+		Account account = null;
+		if (userId == 1) {
+			account = new Account(userId, "admin", "47b7bfb65fa83ac9a71dcb0f6296bb6e", "admin", "admin_nimda@email.com",
+					"Admin", "Nimda", true);
+		} else if (userId == 2) {
+			account = new Account(userId, "peter", "47b7bfb65fa83ac9a71dcb0f6296bb6e", "user",
+					"peter_johnson@email.com", "Peter", "Johnson", false);
+		}
+		LOG.info("Deleted Account: " + account.toString());
+		return account;
 	}
 	
-	
+	@PostMapping("/accounts")
+	public ResultString reset_account(@RequestParam("userId") Integer userId) {
+		return new ResultString("Your password has been sent to your email");
+	}
+
 }
